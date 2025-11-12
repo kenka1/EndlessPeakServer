3efa43b61b8a77c4df4b45f8d817862c9fae098e
Grigorev Alexey <litk1pl@gmail.com>
2025-11-12 09:11:36 +0300

    update base_packet tests

--- src/tests/base_packet_tests.cpp
+++ src/tests/base_packet_tests.cpp
@@ -5,12 +5,18 @@
 TEST(BasePacketTest, Foo)
 {
   ep::net::PacketData packet;
+  EXPECT_EQ(packet.body_.size(), 0);
+  EXPECT_EQ(packet.head_.size_, 0);
 
   packet << 3.14 << 0.5f << 521;
+  EXPECT_EQ(packet.body_.size(), sizeof(double) + sizeof(float) + sizeof(int));
+  EXPECT_EQ(packet.head_.size_, sizeof(double) + sizeof(float) + sizeof(int));
   int i;
   float f;
   double d;
   packet >> i >> f >> d;
+  EXPECT_EQ(packet.body_.size(), 0);
+  EXPECT_EQ(packet.head_.size_, 0);
 
   EXPECT_EQ(i, 521);
   EXPECT_EQ(f, 0.5f);
