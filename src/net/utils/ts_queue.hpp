a2fa34ab632765fa4f6a45b5bfacc05775f3aaf0
Grigorev Alexey <litk1pl@gmail.com>
2025-11-12 14:49:29 +0300

    add threadsafe queue

---
+++ src/net/utils/ts_queue.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+namespace ep::net
+{
+  template<typename T>
+  class TSQueue {
+  public:
+    TSQueue() = default;
+    TSQueue(const TSQueue&) = delete;
+    TSQueue& operator=(const TSQueue&) = delete;
+
+    std::shared_ptr<T> TryPop() noexcept;
+    bool TryPop(T& value);
+
+    std::shared_ptr<T> WaitAndPop() noexcept;
+    void WaitAndPop(T& value);
+
+    void Push(T new_value);
+    void Push(std::shared_ptr<T> new_value);
+    bool Empty() const noexcept;
+    int Size() const noexcept;
+
+  private:
+    mutable std::mutex data_mutex_;
+    std::condition_variable data_cond_;
+    std::queue<std::shared_ptr<T>> data_;
+  };
+
+  template<typename T>
+  std::shared_ptr<T> TSQueue<T>::TryPop() noexcept
+  {
+    std::lock_guard lock(data_mutex_);
+    if (data_.empty())
+      return std::shared_ptr<T>();
+    std::shared_ptr<T> res = std::move(data_.front());
+    data_.pop();
+    return res;
+  }
+
+  template<typename T>
+  bool TSQueue<T>::TryPop(T& value)
+  {
+    std::lock_guard lock(data_mutex_);
+    if (data_.empty())
+      return false;
+    value = std::move(*data_.front());
+    data_.pop();
+    return true;
+  }
+
+  template<typename T>
+  std::shared_ptr<T> TSQueue<T>::WaitAndPop() noexcept
+  {
+    std::unique_lock lock(data_mutex_);
+    data_cond_.wait(lock, [this]{ return !this->data_.empty(); });
+    std::shared_ptr<T> res = std::move(data_.front());
+    data_.pop();
+    return res;
+  }
+
+  template<typename T>
+  void TSQueue<T>::WaitAndPop(T& value)
+  {
+    std::unique_lock lock(data_mutex_);
+    data_cond_.wait(lock, [this]{ return !this->data_.empty(); });
+    value = std::move(*data_.front());
+    data_.pop();
+  }
+
+  template<typename T>
+  void TSQueue<T>::Push(T new_value)
+  {
+    std::shared_ptr<T> data = std::make_shared<T>(std::move(new_value));
+    std::lock_guard lock(data_mutex_);
+    data_.push(data);
+    data_cond_.notify_one();
+  }
+
+  template<typename T>
+  void TSQueue<T>::Push(std::shared_ptr<T> new_value)
+  {
+    std::lock_guard lock(data_mutex_);
+    data_.push(new_value);
+    data_cond_.notify_one();
+  }
+
+  template<typename T>
+  bool TSQueue<T>::Empty() const noexcept
+  {
+    std::lock_guard lock(data_mutex_);
+    return data_.empty();
+  }
+
+  template<typename T>
+  int TSQueue<T>::Size() const noexcept
+  {
+    std::lock_guard lock(data_mutex_);
+    return data_.size();
+  }
+}
---
+++ src/tests/ts_queue_tests.cpp
@@ -0,0 +1,99 @@
+#include <thread>
+#include <functional>
+
+#include <gtest/gtest.h>
+
+#include "utils/ts_queue.hpp"
+using namespace ep::net;
+
+TEST(TSQueue, EmptyTest)
+{
+  TSQueue<int> data;
+  EXPECT_EQ(data.Empty(), true);
+}
+
+TEST(TSQueue, ZeroSizeTest)
+{
+  TSQueue<int> data;
+  EXPECT_EQ(data.Size(), 0);
+}
+
+TEST(TSQueue, PushValTest)
+{
+  TSQueue<int> data;
+  std::thread t([&data]{
+    data.Push(1);
+    data.Push(1);
+    data.Push(1);
+  });
+  t.join();
+  
+  EXPECT_EQ(data.Size(), 3);
+  EXPECT_EQ(data.Empty(), false);
+}
+
+TEST(TSQueue, PushPtrTest)
+{
+  TSQueue<int> data;
+  std::thread t([&data]{
+    std::shared_ptr<int> a(new int(1));
+    data.Push(std::move(a));
+  });
+  t.join();
+  
+  EXPECT_EQ(data.Size(), 1);
+  EXPECT_EQ(data.Empty(), false);
+}
+
+TEST(TSQueue, TryPopPtrTest)
+{
+  TSQueue<int> data;
+  std::shared_ptr<int> value = data.TryPop();
+  EXPECT_EQ(value, std::shared_ptr<int>());
+
+  std::thread t([&data]{
+    data.Push(1);
+  });
+  t.join();
+  value = data.TryPop();
+  EXPECT_EQ(*value, 1);
+}
+
+TEST(TSQueue, TryPopValueTest)
+{
+  TSQueue<int> data;
+  int value;
+  EXPECT_EQ(data.TryPop(value), false);
+
+  std::thread t([&data]{
+    data.Push(1);
+  });
+  t.join();
+  EXPECT_EQ(data.TryPop(value), true);
+  EXPECT_EQ(value, 1);
+}
+
+TEST(TSQueue, WaitAndPopPtrTest)
+{
+  TSQueue<int> data;
+  std::thread t([&data]{
+    std::shared_ptr<int> value = data.WaitAndPop();
+    EXPECT_EQ(*value, 1);
+  });
+  data.Push(1);
+  t.join();
+  EXPECT_EQ(data.Size(), 0);
+}
+
+TEST(TSQueue, WaitAndPopValueTest)
+{
+  TSQueue<int> data;
+  std::thread t([&data]{
+    int value;
+    data.WaitAndPop(value);
+    EXPECT_EQ(value, 1);
+  });
+  data.Push(1);
+  t.join();
+  EXPECT_EQ(data.Size(), 0);
+}
