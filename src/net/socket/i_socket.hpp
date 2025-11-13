bfa42ea7b8ec9d7844830499ad4d7a8234448a57
Grigorev Alexey <litk1pl@gmail.com>
2025-11-13 16:22:35 +0300

    add socket interface

---
+++ src/net/socket/i_socket.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <system_error>
+#include <string>
+
+namespace ep::net
+{
+  class ISocket {
+  public:
+    using ReadHandler = std::function<void(const std::error_code& ec, std::size_t size)>;
+    using CompletionHandler =  std::function<void(const std::error_code& ec)>;
+    virtual ~ISocket() = default;
+    virtual void close() = 0;
+    virtual void async_read_some(uint8_t* buffer, std::size_t limit, ReadHandler handler) = 0;
+    virtual void async_accept(CompletionHandler handler) = 0;
+    virtual void async_handshake(CompletionHandler handler) = 0;
+    virtual std::string string_address() = 0;
+  };
+}
