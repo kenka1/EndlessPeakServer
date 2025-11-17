1aece66bec4ffd027aa3ff8bf25db051b8878e5b
Grigorev Alexey <litk1pl@gmail.com>
2025-11-17 16:23:25 +0300

    add player interface

--- src/game/player/i_player.hpp
+++ src/game/player/i_player.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <cstddef>
+
 namespace ep::game
 {
   class IPlayer {
   public:
     virtual ~IPlayer() = default;
 
-    virtual void Move(float dx, float dy, float dz) = 0;
+    virtual void Move(double dx, double dy, double dz) = 0;
+
+    virtual double GetX() const noexcept = 0;
+    virtual double GetY() const noexcept = 0;
+    virtual double GetZ() const noexcept = 0;
+    virtual std::size_t GetID() const noexcept = 0;
   };
 }
