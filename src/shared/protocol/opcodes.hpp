6792da692c0821da5d3edd059d5a5cdc81cfa094
Grigorev Alexey <litk1pl@gmail.com>
2025-11-20 16:58:36 +0300

    add enum converter

--- src/shared/protocol/opcodes.hpp
+++ src/shared/protocol/opcodes.hpp
@@ -2,12 +2,18 @@
 
 #include <cstdint>
 
-namespace ep::protocol
+namespace ep
 {
   enum class Opcodes : uint16_t {
-    MoveForward             = 0x0001,
-    MoveLeft                = 0x0002,
-    MoveBackward            = 0x0003,
-    MoveRight               = 0x0004,
+    MoveForward             = 0x0011,
+    MoveLeft                = 0x0012,
+    MoveBackward            = 0x0013,
+    MoveRight               = 0x0014,
+    MovePlayer              = 0x0015,
   };
+
+  constexpr uint16_t to_uint16(ep::Opcodes opcode)
+  {
+    return static_cast<uint16_t>(opcode);
+  }
 }
