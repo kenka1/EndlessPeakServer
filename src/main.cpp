7bc6f7543ef1f29cf215bed361639ed079931b36
Grigorev Alexey <litk1pl@gmail.com>
2025-11-25 13:38:52 +0300

    fix dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, char* argv[])
   server->Run();
 
   // Server boradcast all general packets.
-  std::jthread broadcast_tread([server]{ server->Broadcast(); });
+  std::jthread sender_tread([server]{ server->Sender(); });
 
   // Run the I/O service on the requested number of threads.
   std::vector<std::jthread> v;
