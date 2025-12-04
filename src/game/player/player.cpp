954f4986caa4100395160f45beaba3aa3f08dac4
Grigorev Alexey <litk1pl@gmail.com>
2025-12-04 19:33:12 +0300

    add player velocity

--- src/game/player/player.cpp
+++ src/game/player/player.cpp
@@ -2,10 +2,12 @@
 
 namespace ep::game
 {
-  Player::Player(std::size_t id, double x, double y, std::uint8_t width, std::uint8_t height) :
+  Player::Player(std::size_t id, double x, double y, double vel_x, double vel_y, std::uint8_t width, std::uint8_t height) :
     id_(id),
     x_(x),
     y_(y),
+    vel_x_(vel_x),
+    vel_y_(vel_y),
     width_(width),
     height_(height)
   {}
--- src/game/player/player.hpp
+++ src/game/player/player.hpp
@@ -6,7 +6,7 @@ namespace ep::game
 {
   class Player : public IPlayer {
   public:
-    explicit Player(std::size_t id, double x, double y, std::uint8_t width, std::uint8_t height);
+    explicit Player(std::size_t id, double x, double y, double vel_x, double vel_y, std::uint8_t width, std::uint8_t height);
     ~Player() = default;
 
     double GetX() const noexcept override { return x_; }
@@ -16,12 +16,20 @@ namespace ep::game
 
     std::size_t GetID() const noexcept override { return id_; }
 
+    // velocity
+    double GetVelX() const noexcept { return vel_x_; }
+    double GetVelY() const noexcept { return vel_y_; }
+    void SetVelX(double vel_x) noexcept { vel_x_ = vel_x; }
+    void SetVelY(double vel_y) noexcept { vel_y_ = vel_y; }
+
     void Move(double dx, double dy) override;
 
   private:
     std::size_t id_;
     double x_;
     double y_;
+    double vel_x_;
+    double vel_y_;
     std::uint8_t width_;
     std::uint8_t height_;
   };
