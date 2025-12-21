bb12922ed535e3fd0306bda91ca56e8550253487
Grigorev Alexey <litk1pl@gmail.com>
2025-12-21 12:03:09 +0300

    refactoring(protocol): remove unnecessary std::move

--- src/shared/protocol/base_packet.cpp
+++ src/shared/protocol/base_packet.cpp
@@ -14,7 +14,7 @@ namespace ep
     packet.SetPacketType(PacketType::Rpc);
     packet.SetHeadOpcode(to_uint16(Opcodes::CreatePlayer));
     packet << id << x << y << width << height; 
-    return std::move(packet);
+    return packet;
   }
 
   NetPacket AddPlayerPacket(std::size_t id, double x, double y, std::uint8_t width, std::uint8_t height)
@@ -24,7 +24,7 @@ namespace ep
     packet.SetPacketType(PacketType::RpcOthers);
     packet.SetHeadOpcode(to_uint16(Opcodes::AddPlayer));
     packet << id << x << y << width << height;
-    return std::move(packet);
+    return packet;
   }
 
   NetPacket RemovePlayerPacket(std::size_t id)
@@ -34,7 +34,7 @@ namespace ep
     packet.SetPacketType(PacketType::RpcOthers);
     packet.SetHeadOpcode(to_uint16(Opcodes::RemovePlayer));
     packet << id;
-    return std::move(packet);
+    return packet;
   }
 
   NetPacket MovePlayerPacket(std::size_t id, double x, double y)
@@ -44,7 +44,6 @@ namespace ep
     packet.SetPacketType(PacketType::Broadcast);
     packet.SetHeadOpcode(to_uint16(Opcodes::MovePlayer));
     packet << id << x << y;
-    return std::move(packet);
+    return packet;
   }
-
 }
